b72f71157a35b3bec79bb0311ed3b02a9e4b7e2c
rijan dhakal <rijan_dhakal@outlook.com>
2022-10-27 15:52:52 -0500

    This is the barebones working version

---
+++ lua/user/options.lua
@@ -0,0 +1,5 @@
+vim.opt.showmode = true	--This shows the mode that the editor currently is in.
+vim.opt.cursorline = true	-- This shows the current cursor line.
+vim.opt.number = true	-- This sets the number lines to show up.
+vim.opt.relativenumber = false	-- This sets the numberlines to realtive.
+vim.opt.mouse = "a"	-- This makes it possible to use the mouse inside of neovim.
